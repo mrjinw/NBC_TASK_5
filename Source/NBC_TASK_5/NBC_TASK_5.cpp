f98f0df4983519a3d1bf696c5a414934059d4b98
정진우\mrjin <mrjinw@naver.com>
2026-03-24 19:39:37 +0900

    no message

---
+++ Source/NBC_TASK_5/NBC_TASK_5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_TASK_5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_TASK_5, "NBC_TASK_5" );
